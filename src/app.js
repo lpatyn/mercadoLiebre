88fac95a2f15f58b517e9e58d7ab7c05c3e1bbc8
lpatyn <lautaropatyn@outlook.com>
2022-12-07 00:14:51 -0300

    Login and register views added + routing in navbar

--- src/app.js
+++ src/app.js
@@ -7,3 +7,7 @@ app.use(express.static( './public' ));
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 
 app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ));
+
+app.get('/register', (req, res) => res.sendFile( path.resolve(__dirname, './views/register.html') ));
+
+app.get('/login', (req, res) => res.sendFile( path.resolve(__dirname, './views/login.html') ));
