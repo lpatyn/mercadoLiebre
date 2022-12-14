bc6d078c5046e0885dd23af7ebd973f3dad0e95d
lpatyn <lautaropatyn@outlook.com>
2022-12-13 21:17:34 -0300

    Changed port settings and npm commands

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 app.use(express.static( './public' ));
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
 
 app.get('/', (req, res) => res.sendFile( path.resolve(__dirname, './views/index.html') ));
 
